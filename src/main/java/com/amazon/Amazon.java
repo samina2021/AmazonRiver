9fe7b68708def968cf1dd197404609042e3b9583
samina2021 <saminasumi91@gmail.com>
2023-10-31 20:34:14 -0400

    first commit

---
+++ src/main/java/com/amazon/Amazon.java
@@ -0,0 +1,10 @@
+package com.amazon;
+
+public class Amazon {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ src/test/java/com/amazon/AmazonTest.java
@@ -0,0 +1,15 @@
+package com.amazon;
+
+import org.testng.annotations.Test;
+
+public class AmazonTest {
+@Test
+
+public void appTest() {
+	System.out.println("hello"); 
+	
+	
+}
+
+
+}
